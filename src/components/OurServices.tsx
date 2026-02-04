cbfffe79f3c3176629082d4b0c1563e92f934b2b
ikokywu <rikiridwan949@gmail.com>
2026-02-04 18:30:41 +0700

    refactor: adjust data

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,7 +1,7 @@
 import { motion, useInView } from 'motion/react';
 import { useRef } from 'react';
 import type { OurServicesProps, ServiceCardProps } from '../types/services';
-import services from '../const/services';
+import { services } from '../const/services';
 
 const ServiceCard = ({ service, index, onBuySellClick }: ServiceCardProps) => {
   const ref = useRef(null);
