41d3b6b9f16bb8494d45595ddeeaadf02f862353
ikokywu <rikiridwan949@gmail.com>
2026-02-04 19:31:05 +0700

    feat: add data

---
+++ src/const/core.ts
@@ -0,0 +1,7 @@
+const coreInformation = {
+   address: 'Jl. Palasari II, Palasari, Ciater, Subang, West Java 41281',
+   businessName: 'ARFIX SERVICE HANDPHONE',
+   mapsUrl: `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent('ARFIX SERVICE HANDPHONE, Jl. Palasari II, Palasari, Ciater, Subang, West Java 41281')}`,
+}
+
+export { coreInformation}
---
+++ src/const/cta.ts
@@ -0,0 +1,13 @@
+import type { FinalCTAProps } from "../types/cta"
+
+const CTABuySellData: FinalCTAProps = {
+   title: "Siap Menjual HP Lama atau Rusak Anda?",
+   description: "Dapatkan penawaran terbaik hari ini. Gratis konsultasi, tanpa komitmen!"
+}
+
+const CTAHomeData: FinalCTAProps = {
+   title: "Hubungi Kami Sekarang!",
+   description: "Tim kami siap membantu kebutuhan HP Anda. Konsultasi gratis dan ramah!"
+}
+
+export { CTAHomeData, CTABuySellData };
