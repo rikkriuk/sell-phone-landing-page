378f1739daae2b73a2eb2c04a3d8761d40b23847
ikokywu <rikiridwan949@gmail.com>
2026-02-04 18:39:53 +0700

    feat: add buy sell data and type

---
+++ src/const/buysell.ts
@@ -0,0 +1,26 @@
+import { DollarSign, Clock, Shield, Zap } from 'lucide-react';
+
+const benefits = [
+   {
+      icon: DollarSign,
+      title: 'Harga Terbaik',
+      description: 'Dapatkan penawaran harga tertinggi untuk HP bekas Anda',
+   },
+   {
+      icon: Clock,
+      title: 'Proses Cepat',
+      description: 'Transaksi selesai dalam hitungan menit, bayar langsung',
+   },
+   {
+      icon: Shield,
+      title: 'Aman & Terpercaya',
+      description: 'Data pribadi Anda dijamin aman dan dihapus permanen',
+   },
+   {
+      icon: Zap,
+      title: 'Pickup Service',
+      description: 'Kami jemput HP Anda langsung ke lokasi, gratis!',
+   },
+];
+
+export { benefits };
