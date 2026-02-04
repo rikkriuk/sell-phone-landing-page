36750f52e6492dce1f8e1692aec48044d8d1d477
ikokywu <rikiridwan949@gmail.com>
2026-02-04 18:17:12 +0700

    refactor: adjust service section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import CounterHero from "../components/Counter";
+import { OurServices } from "../components/OurServices";
 
 const Home = () => {
+   const [currentPage, setCurrentPage] = useState<'counter' | 'buyback'>('counter');
+   
+   const navigateToBuyback = () => {
+      setCurrentPage('buyback');
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+   };
    return (
       <>
          <div id="hero">
             <CounterHero />
+         </div>
+
+         <div id="services">
+            <OurServices onBuySellClick={navigateToBuyback} />
          </div>
       </>
    )
