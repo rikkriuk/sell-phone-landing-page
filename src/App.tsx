988ce7ad7075ed58dfc4ce63bb1340cc02ed42f1
ikokywu <rikiridwan949@gmail.com>
2026-02-04 17:57:44 +0700

    refactor: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./index.css";
-import Hero from "./components/Hero";
-import WhyChooseUs from "./components/WhyChooseUs";
-import Testimonials from "./components/Testimonials";
-import HowItWorks from "./components/HowItWorks";
-import SupportedDevices from "./components/SupportedDevice";
-import { Products } from "./components/Products";
-import Location from "./components/Location";
-import FinalCTA from "./components/FinalCTA";
 import ScrollToTop from "./components/ScrollToTop";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import SellPhone from "./pages/SellPhone";
 
 const App = () => {
   return (
@@ -17,31 +12,10 @@ const App = () => {
       <Header />
 
       <main className="pt-16">
-        <div id="hero">
-          <Hero />
-        </div>
-
-        <SupportedDevices />
-
-        <HowItWorks />
-
-        <div id="why-choose-us">
-          <WhyChooseUs />
-        </div>
-
-        <div id="products">
-          <Products />
-        </div>
-
-        <div id="testimonials">
-          <Testimonials />
-        </div>
-
-        <div id="location">
-          <Location />
-        </div>
-
-        <FinalCTA />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jual-hp" element={<SellPhone />} />
+        </Routes>
       </main>
 
       <Footer />
