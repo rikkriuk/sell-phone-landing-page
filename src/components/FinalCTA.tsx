6c6084a142c8881324aa66917e013bd6e44dedcc
ikokywu <rikiridwan949@gmail.com>
2026-02-05 20:16:49 +0700

    refactor: adjust list benefit

--- src/components/FinalCTA.tsx
+++ src/components/FinalCTA.tsx
@@ -2,6 +2,7 @@ import { motion } from 'motion/react';
 import { useInView } from 'motion/react';
 import { useRef } from 'react';
 import type { FinalCTAProps } from '../types/cta';
+import { buybackBenefits } from '../const/benefits';
 
 const FinalCTA = (data: FinalCTAProps) => {
    const ref = useRef(null);
@@ -92,24 +93,19 @@ const FinalCTA = (data: FinalCTAProps) => {
                transition={{ duration: 0.5, delay: 0.9 }}
                className="mt-12 flex flex-wrap justify-center gap-8 text-white/80"
             >
-               <div className="flex items-center gap-2">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
-                  </svg>
-                  <span>Transaksi Aman</span>
-               </div>
-               <div className="flex items-center gap-2">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
-                  </svg>
-                  <span>Harga Kompetitif</span>
-               </div>
-               <div className="flex items-center gap-2">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
-                  </svg>
-                  <span>Proses Cepat</span>
-               </div>
+               {
+                  buybackBenefits.map((benefit) => (
+                     <div 
+                        key={benefit.id}
+                        className="flex items-center gap-2"
+                     >
+                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"/>
+                        </svg>
+                        <span>{benefit.title}</span>
+                     </div>
+                  ))
+               }
             </motion.div>
          </div>
       </section>
