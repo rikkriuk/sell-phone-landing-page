3d535530fd374b0a3887f1fc0746ce413896ed4f
ikokywu <rikiridwan949@gmail.com>
2026-02-05 20:10:24 +0700

    refactor: adjust whatsapp text

--- src/const/cta.ts
+++ src/const/cta.ts
@@ -2,12 +2,14 @@ import type { FinalCTAProps } from "../types/cta"
 
 const CTABuySellData: FinalCTAProps = {
    title: "Siap Menjual HP Lama atau Rusak Anda?",
-   description: "Dapatkan penawaran terbaik hari ini. Gratis konsultasi, tanpa komitmen!"
+   description: "Dapatkan penawaran terbaik hari ini. Gratis konsultasi, tanpa komitmen!",
+   whatsappText: "Halo, saya mau jual HP rusak/bekas. Bisa dibantu cek harga?"
 }
 
 const CTAHomeData: FinalCTAProps = {
    title: "Hubungi Kami Sekarang!",
-   description: "Tim kami siap membantu kebutuhan HP Anda. Konsultasi gratis dan ramah!"
+   description: "Tim kami siap membantu kebutuhan HP Anda. Konsultasi gratis dan ramah!",
+   whatsappText: "Halo, saya tertarik berkonsultasi di konter ini, bisa bantu saya?"
 }
 
 export { CTAHomeData, CTABuySellData };
